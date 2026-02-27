530aea54e720659ad582ab74a41b47f9d0620fcf
Davit Stepanyan <davit.stepanyan@jpl.nasa.gov>
2026-02-27 10:07:38 -0800

    New product

--- scripts/generate-blog.js
+++ scripts/generate-blog.js
@@ -9,6 +9,7 @@
  */
 
 // node --env-file=./secrets/.script.env scripts/generate-blog.js --asin=<ASIN>
+// node scripts/generate-sitemap.js
 
 const fs = require('fs');
 const path = require('path');
